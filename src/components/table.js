fb3459e26468d1c6681666f3451cf895fcc2114d
Anton Primachok <vilem772978@gmail.com>
2025-10-30 17:06:29 +0700

    MK1

--- src/components/table.js
+++ src/components/table.js
@@ -12,12 +12,48 @@ export function initTable(settings, onAction) {
     const root = cloneTemplate(tableTemplate);
 
     // @todo: #1.2 —  вывести дополнительные шаблоны до и после таблицы
+    if (Array.isArray(before)) {
+        before.slice().reverse().forEach(subName => {
+            root[subName] = cloneTemplate(subName);
+            root.container.prepend(root[subName].container);
+        });
+    }
+
+    if (Array.isArray(after)) {
+        after.forEach(subName => {
+            root[subName] = cloneTemplate(subName);
+            root.container.append(root[subName].container);
+        });
+    }
+
 
     // @todo: #1.3 —  обработать события и вызвать onAction()
+    root.container.addEventListener('change', () => {
+        onAction(); 
+    });
+
+    root.container.addEventListener('reset', () => {
+        setTimeout(onAction); 
+    });
+
+    root.container.addEventListener('submit', (e) => {
+        e.preventDefault(); 
+        onAction(e.submitter); 
+    });
 
     const render = (data) => {
         // @todo: #1.1 — преобразовать данные в массив строк на основе шаблона rowTemplate
-        const nextRows = [];
+        const nextRows = data.map(item => {
+            const row = cloneTemplate(rowTemplate);
+
+            Object.keys(item).forEach(key => {
+                
+                if (row.elements[key]) {
+                    row.elements[key].textContent = item[key];
+                }
+            });
+            return row.container;
+        });
         root.elements.rows.replaceChildren(...nextRows);
     }
 
